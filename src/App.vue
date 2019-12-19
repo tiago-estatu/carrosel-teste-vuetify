87a33c1e84675deaeb5c178a0265d9c8e8b36221
tiagoestatu <tiagoestatu@gmail.com>
2019-12-19 17:11:13 -0300

    Teste vuetify

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-app>
+		<app-navigator></app-navigator>
+		
+		<v-content>
+			<app-carrosel></app-carrosel>	
+		</v-content>
+
+		<app-footer></app-footer>
+		<p>{{texto}}</p>
+	</v-app>
+</template>
+
+<script>
+import Navigator from '@/componentes/template/Nav.vue'
+import Carrosel from '@/componentes/widgets/Carrosel.vue'
+import Footer from '@/componentes/template/Footer.vue'
+
+export default {
+	components: {
+			'app-navigator':Navigator, 
+			'app-carrosel':Carrosel, 
+			'app-footer':Footer
+		}
+}
+</script>
---
+++ src/componentes/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  
+    <v-footer class="pa-3" app>
+        <v-spacer></v-spacer>
+        <p>{{texto}}</p>
+        <v-spacer></v-spacer>
+        <div>Vue &copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+ 
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            texto : 'Teste Vuetify Carrosel'
+        }
+    }    
+}
+</script>
+
+<style>
+
+</style>
